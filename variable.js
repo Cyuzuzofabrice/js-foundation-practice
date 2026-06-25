63c6b2400e98e505c59b90f6dff65f2180112867
Fabrice Cyuzuzo <fabricecyuzuzo2@gmail.com>
2026-06-25 14:16:55 +0200

    new file added

---
+++ variable.js
@@ -0,0 +1,32 @@
+// variables with datatype of string
+
+let name="cyuzuzo fabrice";
+
+//variable with datatype of number
+
+let age=20;
+
+//variable with datatype of boolean
+
+let vision=true;
+
+// variable with datatype of object
+
+let person={
+    name:"cyuzuzo fabrice",
+    height:1.83,
+    weight:"70kg",
+    dob:"2/04/2006"
+};
+
+//Varriable with datype of array
+
+let cars=["mercedenz benz","BMW","ferrari","cardilac","XUV"];
+
+console.log(name);
+console.log(age);
+console.log(vision);
+console.log(person);
+console.log(cars);
+
+
